ae77e3186ae7eec72a204510b11424d827ca0483
ulion <ulion2002@gmail.com>
2013-05-11 17:35:41 +0800

    [DVDPlayer] ffmpeg support http/https protocol options, also fix #14309.
    Conflicts:
    	xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStream.h

--- xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStream.h
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStream.h
@@ -25,6 +25,8 @@
 #include "filesystem/IFileTypes.h"
 
 #include "FileItem.h"
+#include "URL.h"
+#include "guilib/Geometry.h"
 
 enum DVDStreamType
 {
@@ -143,12 +145,13 @@ public:
   virtual ~CDVDInputStream();
   virtual bool Open(const char* strFileName, const std::string& content);
   virtual void Close() = 0;
-  virtual int Read(BYTE* buf, int buf_size) = 0;
+  virtual int Read(uint8_t* buf, int buf_size) = 0;
   virtual int64_t Seek(int64_t offset, int whence) = 0;
   virtual bool Pause(double dTime) = 0;
   virtual int64_t GetLength() = 0;
   virtual std::string& GetContent() { return m_content; };
   virtual std::string& GetFileName() { return m_strFileName; }
+  virtual CURL &GetURL() { return m_url; }
   virtual ENextStream NextStream() { return NEXTSTREAM_NONE; }
   virtual void Abort() {}
   virtual int GetBlockSize() { return 0; }
@@ -175,6 +178,7 @@ public:
 protected:
   DVDStreamType m_streamType;
   std::string m_strFileName;
+  CURL m_url;
   BitstreamStats m_stats;
   std::string m_content;
   CFileItem m_item;
